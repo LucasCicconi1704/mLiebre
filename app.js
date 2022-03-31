adbe1c81475d4274629271741f11c0c50f1d9803
LucasCicconi1704 <96955667+LucasCicconi1704@users.noreply.github.com>
2022-03-30 21:26:17 -0300

    commit

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
-app.get('/register', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html'));
 });
 
-app.get('/login', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/login.html'));
 });
 
